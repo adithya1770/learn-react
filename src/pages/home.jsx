6e2ae346b421e36144bdb829da5098f7b4a86a3c
adithya1770 <adithyaps929@gmail.com>
2024-06-10 13:02:13 +0530

    test

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -17,12 +17,12 @@ function Home(){
 
     return(
         <div>
-        <div className="poppins-bold absolute lg:bottom-5 lg:pl-160 pl-6 bottom-64">
+        <div className="poppins-bold absolute lg:bottom-5 lg:pl-160 pl-6 bottom-52">
                     <br /><input type="text" placeholder='Enter Feedback' class="text-black"  onChange={(e) => setfeedBack(e.target.value)} className='hover:shadow-2xl text-center placeholder-black h-10 w-64 rounded-full'/> <br />
                     <center><button onClick={sendFeed} className='lg:pl-2 lg:w-20 lg:h-10 text-center lg:text-xl text-white rounded-full'>Send!</button>
                     <p id="Message1" className='text-white' ></p></center>
         </div>
-        <div className='h-24 w-24 bg-white absolute lg:left-170 rounded-2xl top-6 left-32 '>
+        <div className='h-24 w-24 bg-white absolute lg:left-170 rounded-2xl top-6 left-36 '>
         <a href="/"><img src="https://png.pngtree.com/png-clipart/20230209/original/pngtree-banyan-tree-png-image_8948930.png" className='sticky lg:h-24 lg:w-24 lg:top-3 lg:right-5'/></a>
         </div>
             <p className="fonter animated-text absolute lg:text-10xl lg:left-128 lg:top-64 top-80 left-6 text-8xl">Farmy</p>
